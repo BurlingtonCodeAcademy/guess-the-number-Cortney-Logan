dbb579f2f5f9675e182d3f6885543d7cf618aa12
Cortney-Logan <logan.cort@gmail.com>
2021-01-28 16:30:31 -0500

    outlining pseudo code with comments

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface(process.stdin, process.stdout);
 
 function ask(questionText) {
@@ -7,13 +7,35 @@ function ask(questionText) {
   });
 }
 
-start();
-
 async function start() {
-  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
-  let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-  console.log('You entered: ' + secretNumber);
-  // Now try and complete the program
-  //adding a comment to practice committing
+  console.log(
+    "Let's play a game where you (human) make up a number and I (computer) try to guess it."
+  );
+  let readyToPlay = await ask(
+    "Have you picked a random number between 1 and 100? (y/n): "
+  );
+  if (readyToPlay === "y" || readyToPlay === "yes") {
+    console.log("Great, let's get started!");
+  } else {
+    console.log("Ok, I'll wait, please pick a number between 1 and 100.");
+  };
+
+  //keep track of min & max for range of guesses
+  let min = 1;
+  let max = 100;
+
+  //the computer guesses - create a function that guesses a number within the current range
+
+  //compare the number to the actual number
+  // if the computer guessed the correct number ==> user responds 'y' and game gives victory message
+  // else the computer guessed incorrectly ==> user responds 'n' and computer asks if higher or lower
+
+  //users responds h or l and changes min or max respectively
+
+  //computer guesses again, but within the new range
+  //until it breaks - keep guessing
+
   process.exit();
 }
+
+start();
