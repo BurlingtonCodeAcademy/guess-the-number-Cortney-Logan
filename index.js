728537c7f6958591fb4e904257ae972965c5eeb2
Cortney-Logan <logan.cort@gmail.com>
2021-01-28 17:06:02 -0500

    a response of h/l modifies the range

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ function ask(questionText) {
   });
 }
 
-//returns a random guess between the given min and max range
-function makeRandomGuess(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
 async function start() {
+  //returns a random guess between the given min and max range
+  function makeRandomGuess(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  //keep track of min & max for range of guesses
+  let min = 1;
+  let max = 100;
+
   //starter code
   console.log(
     "Let's play a game where you (human) make up a number and I (computer) try to guess it."
@@ -26,10 +29,6 @@ async function start() {
     console.log("Ok, I'll wait, please pick a number between 1 and 100.");
   }
 
-  //keep track of min & max for range of guesses
-  let min = 1;
-  let max = 100;
-
   //the computer makes a guess
   let guess = makeRandomGuess(min, max);
   let response = await ask(`Is the number ${guess}? (y/n): `);
@@ -40,7 +39,20 @@ async function start() {
   }
   //if the computer guessed wrong it asks if the number is higher or lower
   else {
-    console.log(`Bummer.  Is the number higher (h) or lower (l)? `);
+    //stores the h/l response from the user as a variable
+    let modifyRange = await ask(
+      `Bummer.  Is the number higher (h) or lower (l)? `
+    );
+    // if the number is higher, the guess is the new min of the range
+    if (modifyRange === "h") {
+      min = guess;
+      console.log(`the range is now ${min} to ${max}`);
+    }
+    //if the number is lower, the guess is the new max of the range
+    else if (modifyRange === "l") {
+      max = guess;
+      console.log(`the range is now ${min} to ${max}`);
+    }
   }
 
   process.exit();
@@ -49,7 +61,4 @@ async function start() {
 start();
 
 //OUTLINE//
-//users responds h or l and changes min or max respectively
-
 //computer guesses again, but within the new range
-//until it breaks - keep guessing
