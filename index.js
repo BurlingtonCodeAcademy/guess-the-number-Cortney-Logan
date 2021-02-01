44450248b3a3c77654f47a9558a262d19e5c9b07
Cortney-Logan <logan.cort@gmail.com>
2021-01-31 20:20:14 -0500

    adding option to play the game again and updating formatting of outputs

--- index.js
+++ index.js
@@ -54,100 +54,109 @@ async function start() {
     }
   }
 
-  //keep track of min & max for range of guesses
+  //keep track of min & max for range of guesses. Default values are 1 and 100
   let min = 1;
   let max = 100;
 
   //declares wantToPlay variable to allow users to play multiple times
   let wantToPlay = "y";
 
-  //starts the game
-  console.log(
-    "Let's play a game where you (human) pick a number between 1 and a maximum, and I (computer) try to guess it."
-  );
-
-  //while loop here to play the game again....
-  //allow the user to set the high range
-  max = await ask("What would you like the maximum number to be? ");
-
-  //confirms the user is ready to play
-  let readyToPlay = await ask(
-    `Have you decided on a random number between 1 and ${max}? (y/n): `
-  );
-
-  //waits until the player is ready to play by entering 'y' or 'yes'
-  while (readyToPlay !== "y" && readyToPlay !== "yes") {
-    readyToPlay = await ask(
-      `Ok, I'll wait, please pick a number between 1 and ${max}. Are you ready now? (y/n): `
+  //while wantToPlay is yes the game will continue to run.  If the user selects no the game ends
+  while (wantToPlay === "y" || wantToPlay === "yes") {
+    //starts the game
+    console.log(
+      "Let's play a game where you (human) pick a number between 1 and a maximum, and I (computer) try to guess it."
     );
-  }
-  console.log("Great, let's get started!");
 
-  // declares the variable that will store the users response if the computer's guess is correct or not
-  let response = "n";
-  //declares the numOfGuess variable to keep track of the number of guesses
-  let numOfGuess = 0;
+    //while loop here to play the game again....
+    //allow the user to set the high range
+    max = await ask("\nWhat would you like the maximum number to be? ");
 
-  //while the user has not responded 'y' to indicate that the computer has correctly guessed, the computer will continue making guesses
-  while ((response === "n") | (response === "no")) {
-    //sets the computer up to make a random guess within the current range
-    //let guess = makeRandomGuess(min, max);
-    //sets the computer up to make a smart guess within the current range
-    let guess = makeSmartGuess(min, max);
+    //confirms the user is ready to play
+    let readyToPlay = await ask(
+      `\nHave you decided on a random number between 1 and ${max}? (y/n): `
+    );
 
-    //stores the users response if the computer's guess is correct or not
-    response = await ask(`Is the number ${guess}? (y/n): `);
-
-    //the computer has made another guess - index number of guesses made by 1
-    numOfGuess += 1;
-
-    // if the computer guessed the correct number ==> user responds 'y' and game gives victory message
-    if (response === "y" || response === "yes") {
-      console.log(
-        `Aha! Your number was ${guess}! I win!\nIt only took me ${numOfGuess} tries to correctly guess your number.`
+    //waits until the player is ready to play by entering 'y' or 'yes'
+    while (readyToPlay !== "y" && readyToPlay !== "yes") {
+      readyToPlay = await ask(
+        `\nOk, I'll wait, please pick a number between 1 and ${max}. Are you ready now? (y/n): `
       );
     }
-    //if the computer guessed wrong it asks if the number is higher or lower
-    else {
-      //if the min, max and guess are all equal then the computer has correctly narrowed down the number and the user is cheating
-      if (cheatDetector(min, max, guess, "")) {
-        console.log("Please be honest this time....");
-        //since numOfGuess will iterate once more when the computer prompts the users again we need to walk it down by 1 to correctly indicate the number of guesses taken
-        numOfGuess -= 1;
-      } else {
-        console.log("Bummer.");
+    console.log("Great, let's get started!");
 
-        //declare the variable modifyRange that will hold h/l
-        let modifyRange = "";
+    // declares the variable that will store the users response if the computer's guess is correct or not
+    let response = "n";
+    //declares the numOfGuess variable to keep track of the number of guesses
+    let numOfGuess = 0;
 
-        while (!modifyRange) {
-          //stores the h/l response from the user in modifyRange
-          modifyRange = await ask(
-            `Is the number higher (h) or lower (l) than ${guess}? `
-          );
-          // if the number is higher, the guess+1 is the new min of the range
-          if (modifyRange === "h") {
-            if (cheatDetector(min, max, guess, modifyRange)) {
-              console.log("Please tell me the truth this time...");
-              modifyRange = "";
-            } else {
-              min = guess + 1;
+    //while the user has not responded 'y' to indicate that the computer has correctly guessed, the computer will continue making guesses
+    while ((response === "n") | (response === "no")) {
+      //sets the computer up to make a random guess within the current range
+      //let guess = makeRandomGuess(min, max);
+      //sets the computer up to make a smart guess within the current range
+      let guess = makeSmartGuess(min, max);
+
+      //stores the users response if the computer's guess is correct or not
+      response = await ask(`\nIs the number ${guess}? (y/n): `);
+
+      //the computer has made another guess - index number of guesses made by 1
+      numOfGuess += 1;
+
+      // if the computer guessed the correct number ==> user responds 'y' and game gives victory message
+      if (response === "y" || response === "yes") {
+        console.log(
+          `Aha! Your number was ${guess}! I win!\nIt only took me ${numOfGuess} tries to correctly guess your number.`
+        );
+
+        wantToPlay = await ask("\nWould you like to play again? (y/n): ");
+
+        if (wantToPlay === "n" || wantToPlay === "no") {
+          console.log("\nOk, goodbye!");
+          process.exit();
+        }
+      }
+      //if the computer guessed wrong it asks if the number is higher or lower
+      else {
+        //if the min, max and guess are all equal then the computer has correctly narrowed down the number and the user is cheating
+        if (cheatDetector(min, max, guess, "")) {
+          console.log("Please be honest this time....");
+          //since numOfGuess will iterate once more when the computer prompts the users again we need to walk it down by 1 to correctly indicate the number of guesses taken
+          numOfGuess -= 1;
+        } else {
+          console.log("Bummer.");
+
+          //declare the variable modifyRange that will hold h/l
+          let modifyRange = "";
+
+          while (!modifyRange) {
+            //stores the h/l response from the user in modifyRange
+            modifyRange = await ask(
+              `Is the number higher (h) or lower (l) than ${guess}? `
+            );
+            // if the number is higher, the guess+1 is the new min of the range
+            if (modifyRange === "h") {
+              if (cheatDetector(min, max, guess, modifyRange)) {
+                console.log("Please tell me the truth this time...");
+                modifyRange = "";
+              } else {
+                min = guess + 1;
+              }
             }
-          }
-          //if the number is lower, the guess-1 is the new max of the range
-          else if (modifyRange === "l") {
-            if (cheatDetector(min, max, guess, modifyRange)) {
-              console.log("Please tell me the truth this time...");
-              modifyRange = "";
-            } else {
-              max = guess - 1;
+            //if the number is lower, the guess-1 is the new max of the range
+            else if (modifyRange === "l") {
+              if (cheatDetector(min, max, guess, modifyRange)) {
+                console.log("Please tell me the truth this time...");
+                modifyRange = "";
+              } else {
+                max = guess - 1;
+              }
             }
           }
         }
       }
     }
   }
-  process.exit();
 }
 
 start();
