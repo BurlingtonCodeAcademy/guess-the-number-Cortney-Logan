fd62a65602e62440606a5d8216f3d8534accdda4
Cortney-Logan <logan.cort@gmail.com>
2021-01-31 20:37:55 -0500

    allows user to play the game again

--- reverse_game.js
+++ reverse_game.js
@@ -13,39 +13,46 @@ async function start() {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
+  //intros the game
   console.log(
     "Let's play a game where I (computer) pick a number between 1 and 100, and you (human) try to guess it."
   );
 
-  //the computer picks a random number between 1 and 100
-  let randomNumber = chooseRandomNumber(1, 100);
-  //declare the variable to hold the user's guess
-  let guess = 0;
+  let wantToPlay = "y";
+  while (wantToPlay === "y" || wantToPlay === "yes") {
+    //the computer picks a random number between 1 and 100
+    let randomNumber = chooseRandomNumber(1, 100);
+    //declare the variable to hold the user's guess
+    let guess = 0;
 
-  console.log("I have picked a random number between 1 and 100.");
+    console.log("\nI have picked a random number between 1 and 100.");
 
-  //allows the user to guess as long as guess is not equal to the chosen randomNumber
-  while (randomNumber !== +guess) {
-    //prompts the use for a guess
-    guess = await ask("Please make a guess: ");
-    //if the guess is less than the randomNumber indicates that the guess is too low
-    if (randomNumber > +guess) {
-      console.log("You guessed too low.");
-    }
-    //if the guess is greater than the random number indicates that the guess is too high
-    else if (randomNumber < +guess) {
-      console.log("You guessed too high.");
-    }
-    //only triggered if the guess is equal to the randomNumber
-    else {
-      console.log(
-        `Congratulations! You correctly guessed that the number is ${randomNumber}.`
-      );
-      break;
+    //allows the user to guess as long as guess is not equal to the chosen randomNumber
+    while (randomNumber !== +guess) {
+      //prompts the use for a guess
+      guess = await ask("\nPlease make a guess: ");
+      //if the guess is less than the randomNumber indicates that the guess is too low
+      if (randomNumber > +guess) {
+        console.log("\nYou guessed too low.");
+      }
+      //if the guess is greater than the random number indicates that the guess is too high
+      else if (randomNumber < +guess) {
+        console.log("\nYou guessed too high.");
+      }
+      //only triggered if the guess is equal to the randomNumber
+      else {
+        console.log(
+          `\nCongratulations! You correctly guessed that the number is ${randomNumber}.`
+        );
+        wantToPlay = await ask("\nWould you like to play again? (y/n): ");
+
+        if (wantToPlay === "n" || wantToPlay === "no") {
+          console.log("\nGoodbye, thanks for playing!");
+          process.exit();
+        }
+      }
     }
   }
-  process.exit();
 }
 
-//adding a comment
 start();
